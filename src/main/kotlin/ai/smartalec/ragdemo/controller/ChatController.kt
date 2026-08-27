87b9662b1227d8b6e4e2b620fcdfd6da5360c1e3
Alec Ferguson <alec.ferguson@gmail.com>
2026-08-26 18:20:22 -0700

    ktlint

--- src/main/kotlin/ai/smartalec/ragdemo/controller/ChatController.kt
+++ src/main/kotlin/ai/smartalec/ragdemo/controller/ChatController.kt
@@ -1,17 +1,22 @@
 package ai.smartalec.ragdemo.controller
 
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.http.MediaType.TEXT_PLAIN
 import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ChatController(private val chatClient: ChatClient) {
-
+class ChatController(
+    private val chatClient: ChatClient,
+) {
     @PostMapping("/chat", consumes = [TEXT_PLAIN_VALUE], produces = [TEXT_PLAIN_VALUE])
-    fun chat(@RequestBody chatMessage: String): String? {
-        return chatClient.prompt().user(chatMessage).call().content()
-    }
+    fun chat(
+        @RequestBody chatMessage: String,
+    ): String? =
+        chatClient
+            .prompt()
+            .user(chatMessage)
+            .call()
+            .content()
 }
--- src/test/kotlin/ai/smartalec/ragdemo/RetrievalAugmentedGenerationDemoApplicationTests.kt
+++ src/test/kotlin/ai/smartalec/ragdemo/RetrievalAugmentedGenerationDemoApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class RetrievalAugmentedGenerationDemoApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
