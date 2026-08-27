6bbb489249f365bef4835b2f79ad5158a60cc5d8
Alec Ferguson <alec.ferguson@gmail.com>
2026-08-26 17:49:34 -0700

    Endpoint

--- src/main/kotlin/ai/smartalec/ragdemo/Application.kt
+++ src/main/kotlin/ai/smartalec/ragdemo/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RetrievalAugmentedGenerationDemoApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<RetrievalAugmentedGenerationDemoApplication>(*args)
+	runApplication<Application>(*args)
 }
---
+++ src/main/kotlin/ai/smartalec/ragdemo/controller/ChatController.kt
@@ -0,0 +1,17 @@
+package ai.smartalec.ragdemo.controller
+
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.http.MediaType.TEXT_PLAIN
+import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ChatController(private val chatClient: ChatClient) {
+
+    @PostMapping("/chat", consumes = [TEXT_PLAIN_VALUE], produces = [TEXT_PLAIN_VALUE])
+    fun chat(@RequestBody chatMessage: String): String? {
+        return chatClient.prompt().user(chatMessage).call().content()
+    }
+}
